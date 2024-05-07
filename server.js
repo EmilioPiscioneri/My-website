68f2426eff9bff8b5380b6cfad415af5cf588275
EmilioPiscioneri <emilio.piscioneri@gmail.com>
2024-05-07 17:51:14 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5123; // port for website
+const openUrl = require("openurl").open;
+
+// make public folder, static
+app.use(express.static('public'))
+
+// redirect to home
+app.get('/', (req, res) => {
+  res.redirect("/home");
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname+'/public/home/main-site.html')
+  })
+
+  app.listen(port, () => {
+    openUrl("http://localhost:5123")
+  console.log(`Example app listening on port ${port}`)
+})
